cd0bfcf059b61cab9a3343f78d6ac961547adfda
Cody Kasselman <ckasselman@fordham.edu>
2017-10-26 01:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function createPost(){
-  let postTemplate = document.getElementById('post-template').innerHTML
-  let postTemplateFn = _.template(postTemplate)
-  let pageTemplate = document.getElementById('page-template').innerHTML
-  let pageTemplateFn = _.template(pageTemplate)
-}
+  let postTitle = document.getElementById("postTitle").value;
+  let postBody = document.getElementById("postBody").value;
+  let postAuthor = document.getElementById("postAuthor").value;
+  let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  let postDiv = document.getElementById("post");
+  let postHTML = postTemplate({
+    'postTitle': postTitle,
+    'postBody': postBody,
+    'postAuthor': postAuthor
+  })
+
+  postDiv.innerHTML += postHTML;
+  postDiv.getElementsByTagName("footer")[0].innerHTML = commentsTemplate();
+}  
 
 function postComment(){
-  let commentTemplate = document.getElementById('comment-template').innerHTML
-  let commentTemplateFn = _.template(commentTemplate)
+  let comment = document.getElementById("comment").value;
+  let commenter = document.getElementById("commenter").value;
+  let commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+  let commentHTML = commentTemplate({
+    'comment': comment,
+    'commenter': commenter
+  })
+  let commentsDiv = document.getElementById("comments");
+  
+  commentsDiv.innerHTML += commentHTML;
 }
